9bc825de0786ff15fca17330d9e3c7bb129cce2d
Jack Anderson <whotfisjax@gmail.com>
2024-09-15 18:34:23 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ function App() {
         </div>
       ) : (
         <div className="w-full h-full">
+          <div className='absolute bottom-0 w-full bg-gray-400 h-min py-1 flex justify-center text-xs md:text-base'>CA: updating...</div>
           <div className="flex h-screen bg-gray-950">
             {/* Sidebar */}
             <aside className="w-64 bg-gray-950 border-r border-gray-600">
@@ -64,13 +65,13 @@ function App() {
                   </svg>
                   <span className="mx-4 font-medium">Cases</span>
                 </a>
-                <a href="" className="flex items-center px-4 py-2 mt-2 text-gray-300 hover:bg-gray-400">
+                <a href="https://t.me/AdogPortal" className="flex items-center px-4 py-2 mt-2 text-gray-300 hover:bg-gray-400">
                   <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                   </svg>
                   <span className="mx-4 font-medium">Telegram</span>
                 </a>
-                <a href="" className="flex items-center px-4 py-2 mt-2 text-gray-300 hover:bg-gray-400">
+                <a href="https://x.com/aDOGsolana" className="flex items-center px-4 py-2 mt-2 text-gray-300 hover:bg-gray-400">
                   <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                   </svg>
